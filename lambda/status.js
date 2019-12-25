b6ef2c3b0deffd6fc806c5e18e58b44d2ce6cd4f
zlargon <zlargon1988@gmail.com>
2019-12-24 23:30:58 -0800

    integrate with uscis API

--- lambda/status.js
+++ lambda/status.js
@@ -1,10 +1,30 @@
+import fetch from 'node-fetch';
+import cheerio from 'cheerio';
+
 export async function main(event) {
   const { receipt_number } = event.pathParameters;
 
+  const res = await fetch('https://egov.uscis.gov/casestatus/mycasestatus.do', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Referer': 'https://egov.uscis.gov/casestatus/landing.do',
+    },
+    body: `changeLocale=&appReceiptNum=${receipt_number}&initCaseSearch=CHECK+STATUS`
+  });
+
+  const html = await res.text();
+  const $ = cheerio.load(html);
+  let result = $('div.rows.text-center').html();
+  if (result === null) {
+    result = html;
+  }
+
   return {
-    statusCode: 200,
-    body: JSON.stringify({
-      receipt_number
-    })
+    statusCode: res.status,
+    headers: {
+      'Content-Type': 'text/html'
+    },
+    body: result
   };
-}
+};
